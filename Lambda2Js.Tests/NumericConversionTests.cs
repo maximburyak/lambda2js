2a8c5ffe1c9e4001bf2d2317e65d5908abc79840
maximburyak <maxim@ayende.com>
2017-03-12 14:12:46 +0200

    Numeric conventions - WIP

---
+++ Lambda2Js.Tests/NumericConversionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Lambda2Js.Plugins;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lambda2Js.Tests
+{
+    [TestClass]
+    public class NumericConversionTests
+    {
+        [TestMethod]
+        public void ParseTo_int32()
+        {
+            Expression<Func<MyClass, object>> expr =
+                 x => x.Phones.Where(y=>y.DDD == 4).Select(z=>z.DDD);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new NumericConversions()));
+            Assert.AreEqual(@"x.Name.length", js);
+        }
+        
+    }
+}
---
+++ Lambda2Js/Plugins/NumericConversions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Lambda2Js.Plugins
+{
+    public class NumericConversions: JavascriptConversionExtension
+    {
+        public override void ConvertToJavascript(JavascriptConversionContext context)
+        {
+            var writer = context.GetWriter();
+
+            var methodCall = context.Node as System.Linq.Expressions.MethodCallExpression;
+            if (methodCall != null)
+            {
+                ProccessMethodCall(context, writer, methodCall);
+                return;
+            }
+
+
+        }
+
+        private void ProccessMethodCall(JavascriptConversionContext context, JavascriptWriter writer, MethodCallExpression methodCall)
+        {
+            if (methodCall.Object == null)
+            {
+                if (methodCall.Method.Name == "Parse")
+                {
+                    if (methodCall.Type == typeof(Int16) || methodCall.Type == typeof(Int32)
+                        || methodCall.Type == typeof(Int64))
+                    {
+                        writer.Write("parseInt(");
+                        context.Visitor.Visit(methodCall.Arguments[0]);
+                        writer.Write(")");
+                        context.PreventDefault();
+                    }
+                    else if (methodCall.Type == typeof(float) || methodCall.Type == typeof(double)
+                             || methodCall.Type == typeof(decimal))
+                    {
+                        writer.Write("parseFloat(");
+                        context.Visitor.Visit(methodCall.Arguments[0]);
+                        writer.Write(")");
+                        context.PreventDefault();
+                    }
+                }
+            }
+            
+        }
+    }
+}
