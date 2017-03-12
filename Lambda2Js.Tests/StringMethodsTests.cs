193214585f3e551d9c8aacaeb4397ae7a442b725
maximburyak <maxim@ayende.com>
2017-03-12 11:59:13 +0200

    Added support to some of the string "instance" functions

---
+++ Lambda2Js.Tests/StringMethodsTests.cs
@@ -0,0 +1,363 @@
+﻿using Lambda2Js.Plugins;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda2Js.Tests
+{
+    [TestClass]
+    public class StringMethodsTests
+    {
+        [TestMethod]
+        public void StringLength()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.Length;
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.length", js);
+        }
+
+        [TestMethod]
+        public void SubstringOneParam()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.Substring(1,5);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(1,5)", js);
+        }
+
+        [TestMethod]
+        public void SubstringTwoParams()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.Substring(5);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(5)", js);
+        }
+
+        [TestMethod]
+        public void ReplaceChars()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.Replace("a","b");
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.replace(""a"",""b"")", js);
+        }
+
+        [TestMethod]
+        public void ReplaceStrings()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.Replace('a', 'b');
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.replace(""a"",""b"")", js);
+        }
+
+        [TestMethod]
+        public void IndexOf1()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.IndexOf('a');
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.indexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void IndexOf2()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.IndexOf("a");
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.indexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void IndexOf3()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.IndexOf('a',2);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(2).indexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void IndexOf4()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.IndexOf("a", 2);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(2).indexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void IndexOf5()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.IndexOf("a", StringComparison.CurrentCulture);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.indexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void IndexOf6()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.IndexOf("a", 2, StringComparison.InvariantCulture);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(2).indexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void IndexOf7()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.IndexOf("a", 2,2);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(2,2).indexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void IndexOf8()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.IndexOf('a', 2, 2);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(2,2).indexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void IndexOf9()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.IndexOf("a", 2,6, StringComparison.InvariantCulture);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(2,6).indexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void Contains()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.Contains("a");
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.includes(""a"")", js);
+        }
+
+        [TestMethod]
+        public void Trim()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.Trim();
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.trim()", js);
+        }
+
+        [TestMethod]
+        public void StartsWith1()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.StartsWith("a");
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.startsWith(""a"")", js);
+        }
+
+        [TestMethod]
+        public void StartsWith2()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.StartsWith("a",StringComparison.InvariantCultureIgnoreCase);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.startsWith(""a"")", js);
+        }
+
+        [TestMethod]
+        public void EndsWith1()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.EndsWith("a");
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.endsWith(""a"")", js);
+        }
+
+        [TestMethod]
+        public void EndsWith2()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.EndsWith("a",StringComparison.Ordinal);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.endsWith(""a"")", js);
+        }       
+
+        [TestMethod]
+        public void LastIndexOf1()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.LastIndexOf('a');
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.lastIndexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void LastIndexOf2()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.LastIndexOf("a");
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.lastIndexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void LastIndexOf3()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.LastIndexOf('a', 2);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(2).lastIndexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void LastIndexOf4()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.LastIndexOf("a", 2);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(2).lastIndexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void LastIndexOf5()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.LastIndexOf("a", StringComparison.CurrentCulture);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.lastIndexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void LastIndexOf6()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.LastIndexOf("a", 2, StringComparison.InvariantCulture);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(2).lastIndexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void LastIndexOf7()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.LastIndexOf("a", 2, 2);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(2,2).lastIndexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void LastIndexOf8()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.LastIndexOf('a', 2, 2);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(2,2).lastIndexOf(""a"")", js);
+        }
+
+        [TestMethod]
+        public void LastIndexOf9()
+        {
+            Expression<Func<MyClass, object>> expr =
+                x => x.Name.LastIndexOf("a", 2, 6, StringComparison.InvariantCulture);
+
+            var js = expr.CompileToJavascript(new JavascriptCompilationOptions(
+                    JsCompilationFlags.BodyOnly,
+                    new StringMethodsAndProperties()));
+            Assert.AreEqual(@"x.Name.substr(2,6).lastIndexOf(""a"")", js);
+        }
+    }
+}
---
+++ Lambda2Js/Plugins/StringMethodsAndProperties.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Lambda2Js.Plugins
+{
+    public class StringMethodsAndProperties : JavascriptConversionExtension
+    {
+        public override void ConvertToJavascript(JavascriptConversionContext context)
+        {
+            var unary = context.Node as UnaryExpression;
+            var writer = context.GetWriter();
+            if (unary != null)
+            {
+                ProccessUnary(context, unary, writer);
+                return;
+            }
+
+            var methodCall = context.Node as System.Linq.Expressions.MethodCallExpression;
+            if (methodCall != null)
+            {
+                ProccessMethodCall(context, writer, methodCall);
+                return;
+            }
+
+
+        }
+
+        private static void ProccessMethodCall(JavascriptConversionContext context, JavascriptWriter writer, MethodCallExpression methodCall)
+        {
+            if (methodCall.Object.Type == typeof(string))
+            {
+                if (methodCall.Method.Name == nameof(string.Substring))
+                {
+                    ProccessSubstring(context, writer, methodCall);
+                }
+                else if (methodCall.Method.Name == nameof(string.Replace))
+                {
+                    ProccessReplace(context, writer, methodCall);
+                }
+                else if (methodCall.Method.Name == nameof(string.IndexOf))
+                {
+                    ProccessIndexOf(context, writer, methodCall);
+                }
+                else if (methodCall.Method.Name == nameof(string.LastIndexOf))
+                {
+                    ProccessLastIndexOf(context, writer, methodCall);
+                }
+                else if (methodCall.Method.Name == nameof(string.Contains))
+                {
+                    ProccessContains(context, writer, methodCall);
+                }
+                else if (methodCall.Method.Name == nameof(string.Trim) && methodCall.Arguments.Count == 0)
+                {
+                    ProccessTrim(context, writer, methodCall);
+                }
+                else if (methodCall.Method.Name == nameof(string.StartsWith) && methodCall.Arguments.Count <= 2)
+                {
+                    ProccessStartsWith(context, writer, methodCall);
+                }
+                else if (methodCall.Method.Name == nameof(string.EndsWith) && methodCall.Arguments.Count <= 2)
+                {
+                    ProccessEndsWith(context, writer, methodCall);
+                }
+                
+
+            }
+        }
+
+        private static void ProccessEndsWith(JavascriptConversionContext context, JavascriptWriter writer, MethodCallExpression methodCall)
+        {
+            context.Visitor.Visit(methodCall.Object);
+            writer.Write(".endsWith(");
+            context.Visitor.Visit(methodCall.Arguments[0]);
+            writer.Write(")");
+            context.PreventDefault();
+        }
+
+        private static void ProccessStartsWith(JavascriptConversionContext context, JavascriptWriter writer, MethodCallExpression methodCall)
+        {
+            context.Visitor.Visit(methodCall.Object);
+            writer.Write(".startsWith(");
+            context.Visitor.Visit(methodCall.Arguments[0]);
+            writer.Write(")");
+            context.PreventDefault();
+        }
+
+        private static void ProccessTrim(JavascriptConversionContext context, JavascriptWriter writer, MethodCallExpression methodCall)
+        {
+            context.Visitor.Visit(methodCall.Object);
+            writer.Write(".trim()");
+            context.PreventDefault();
+        }
+
+        private static void ProccessContains(JavascriptConversionContext context, JavascriptWriter writer, MethodCallExpression methodCall)
+        {
+            context.Visitor.Visit(methodCall.Object);
+            writer.Write(".includes(");
+            context.Visitor.Visit(methodCall.Arguments[0]);
+            writer.Write(")");
+            context.PreventDefault();
+        }
+
+        private static void ProccessIndexOf(JavascriptConversionContext context, JavascriptWriter writer, MethodCallExpression methodCall)
+        {
+            context.Visitor.Visit(methodCall.Object);
+            if (methodCall.Arguments.Count == 1 ||
+                methodCall.Arguments.Count == 2 && methodCall.Arguments[1].Type == typeof(StringComparison))
+            {
+                writer.Write(".indexOf(");
+                context.Visitor.Visit(methodCall.Arguments[0]);
+                writer.Write(")");
+                context.PreventDefault();
+            }
+            else if (methodCall.Arguments.Count == 2 ||
+                methodCall.Arguments.Count == 3 && methodCall.Arguments[2].Type == typeof(StringComparison))
+            {
+                writer.Write(".substr(");
+                context.Visitor.Visit(methodCall.Arguments[1]);
+                writer.Write(")");
+                writer.Write(".indexOf(");
+                context.Visitor.Visit(methodCall.Arguments[0]);
+                writer.Write(")");
+                context.PreventDefault();
+            }
+            else if (methodCall.Arguments.Count == 3 ||
+                methodCall.Arguments.Count == 4 && methodCall.Arguments[3].Type == typeof(StringComparison))
+            {
+                writer.Write(".substr(");
+                context.Visitor.Visit(methodCall.Arguments[1]);
+                writer.Write(",");
+                context.Visitor.Visit(methodCall.Arguments[2]);
+                writer.Write(")");
+                writer.Write(".indexOf(");
+                context.Visitor.Visit(methodCall.Arguments[0]);
+                writer.Write(")");
+                context.PreventDefault();
+            }
+        }
+
+        private static void ProccessLastIndexOf(JavascriptConversionContext context, JavascriptWriter writer, MethodCallExpression methodCall)
+        {
+            context.Visitor.Visit(methodCall.Object);
+            if (methodCall.Arguments.Count == 1 ||
+                methodCall.Arguments.Count == 2 && methodCall.Arguments[1].Type == typeof(StringComparison))
+            {
+                writer.Write(".lastIndexOf(");
+                context.Visitor.Visit(methodCall.Arguments[0]);
+                writer.Write(")");
+                context.PreventDefault();
+            }
+            else if (methodCall.Arguments.Count == 2 ||
+                methodCall.Arguments.Count == 3 && methodCall.Arguments[2].Type == typeof(StringComparison))
+            {
+                writer.Write(".substr(");
+                context.Visitor.Visit(methodCall.Arguments[1]);
+                writer.Write(")");
+                writer.Write(".lastIndexOf(");
+                context.Visitor.Visit(methodCall.Arguments[0]);
+                writer.Write(")");
+                context.PreventDefault();
+            }
+            else if (methodCall.Arguments.Count == 3 ||
+                methodCall.Arguments.Count == 4 && methodCall.Arguments[3].Type == typeof(StringComparison))
+            {
+                writer.Write(".substr(");
+                context.Visitor.Visit(methodCall.Arguments[1]);
+                writer.Write(",");
+                context.Visitor.Visit(methodCall.Arguments[2]);
+                writer.Write(")");
+                writer.Write(".lastIndexOf(");
+                context.Visitor.Visit(methodCall.Arguments[0]);
+                writer.Write(")");
+                context.PreventDefault();
+            }
+        }
+
+        private static void ProccessReplace(JavascriptConversionContext context, JavascriptWriter writer, MethodCallExpression methodCall)
+        {
+            context.Visitor.Visit(methodCall.Object);
+            writer.Write(".replace(");
+            context.Visitor.Visit(methodCall.Arguments[0]);
+            writer.Write(",");
+            context.Visitor.Visit(methodCall.Arguments[1]);
+            writer.Write(")");
+            context.PreventDefault();
+        }
+
+        private static void ProccessSubstring(JavascriptConversionContext context, JavascriptWriter writer, MethodCallExpression methodCall)
+        {
+            context.Visitor.Visit(methodCall.Object);
+            if (methodCall.Arguments.Count == 1)
+            {
+                writer.Write(".substr(");
+                context.Visitor.Visit(methodCall.Arguments[0]);               
+                writer.Write(")");
+                context.PreventDefault();
+            }
+            else
+            {
+                writer.Write(".substr(");
+                context.Visitor.Visit(methodCall.Arguments[0]);
+                writer.Write(",");
+                context.Visitor.Visit(methodCall.Arguments[1]);
+                writer.Write(")");
+                context.PreventDefault();
+            }
+        }
+
+        private static void ProccessUnary(JavascriptConversionContext context, UnaryExpression unary, JavascriptWriter writer)
+        {
+            var operand = unary.Operand as MemberExpression;
+
+            if (operand != null)
+            {
+                if (operand.Expression.Type == typeof(string))
+                {
+                    if (operand.Member.Name == "Length")
+                    {
+                        context.Visitor.Visit(operand.Expression);
+                        writer.Write(".length");
+                        context.PreventDefault();
+                    }
+                }
+            }
+        }
+    }
+}
